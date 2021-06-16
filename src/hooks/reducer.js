a0d320964ee871e905552491946c9facbd7420fc
Guillermo <grlara9@gmail.com>
2021-06-16 00:11:25 -0500

    finish all 3 cases to handle logic

--- src/hooks/reducer.js
+++ src/hooks/reducer.js
@@ -2,8 +2,34 @@ import { useReducer } from "react";
 
 
 export const initialState ={
-
+    loading: true,
+    movies:[],
+    errorMsj: null
 }
-const reducer =()=>{
 
+const reducer = (state, action) =>{
+    switch (action.type){
+        case "SEARCH_MOVIE_REQUEST":
+            return{
+                ...state,
+                loading: true,
+                errorMsj: null
+            };
+            case "SEARCH_MOVIES_REQUEST":
+                return{
+                    ...state,
+                    loading: false,
+                    movies: action.payload
+
+                };
+                case "SEARCH_MOVIES_FAILURE":
+                    return{
+                        ...state,
+                        loading: false,
+                        errorMsj: action.error
+                    };
+                    default:
+                        return state;
+    }
 }
+export default reducer
