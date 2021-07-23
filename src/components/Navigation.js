730fa8d76e87d19c3de30a75ec5bea60f75a0329
Guillermo <grlara9@gmail.com>
2021-07-23 01:22:43 -0500

    Navigation component created

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navigation =()=>{
+    return(
+        <nav className="navbar">
+            <ul>
+                
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
