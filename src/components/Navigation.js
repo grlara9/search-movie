69846f6762f9a1e3ec63ede656b48df30c5f092f
Guillermo <grlara9@gmail.com>
2021-07-25 20:42:41 -0500

    styling css navbar a little bit off

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,37 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import {FiMenu, FiX} from 'react-icons/fi'
+import './Header.css'
 
 const Navigation =()=>{
 
     const [open, setOpen] = useState(false)
+
+    const handleClick = () => {
+		setOpen(!open);
+	};
+
+	const closeMenu = () => {
+		setOpen(false);
+	};
+
     return(
 
         <nav className="navbar">
+            <div onClick={handleClick} className="nav-icon">
+				{open ? <FiX /> : <FiMenu />}
+			</div>
+
             <ul className={open ? 'nav-links active' : 'nav-links'}>
                 <li className="nav-item">
-                    <Link to="/" className="nav-link" onClick={() => StereoPannerNode(false)}>
+                    <Link to="/" className="nav-link" onClick={closeMenu}>
                         Home
                     </Link>
                     
                 </li>
 
                 <li className="nav-item">
-                    <Link to="/favorites" className="nav-link"  onClick={() => StereoPannerNode(false)}>
+                    <Link to="/favorites" className="nav-link"  onClick={closeMenu}>
                         Favorites
                     </Link>
                     
